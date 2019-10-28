8bb4326c9fe5f7c1f987735199922952812750f5
yasu89 <19990967+yasu89@users.noreply.github.com>
2019-10-28 22:05:03 +0900

    トークンをランダムな文字列になるよう実装

--- src/main/kotlin/com/example/backend/controller/UserController.kt
+++ src/main/kotlin/com/example/backend/controller/UserController.kt
@@ -8,11 +8,24 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 import java.security.MessageDigest
 import java.util.Date
+import kotlin.random.Random
 
 @RestController
 class UserController {
     @Autowired
     lateinit var userServiceImpl : UserServiceImpl
+
+    private var TOKEN_LENGTH = 32
+    private val charPool : List<Char> = ('a'..'z') + ('A'..'Z') + ('0'..'9')
+
+    fun createToken(): String{
+        var randomString = (1..TOKEN_LENGTH)
+                .map { _ -> Random.nextInt(0, charPool.size) }
+                .map(charPool::get)
+                .joinToString("")
+
+        return randomString
+    }
 
     @PostMapping("api/v1/user/register")
     fun registUser(@RequestBody user: User): User {
@@ -25,7 +38,7 @@ class UserController {
                     "%02x".format(it)
                 }
         // トークンを作成
-        user.token = "TEST_TOKEN"
+        user.token = createToken()
         var saveUser = userServiceImpl.save(user)
 //        saveUser.password = null
         return saveUser
