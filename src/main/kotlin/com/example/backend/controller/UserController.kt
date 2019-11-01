7b16083b9e4082c1a0ceeffb2c188a9206fa1b74
yasu89 <19990967+yasu89@users.noreply.github.com>
2019-11-02 01:55:58 +0900

    リファクタリング

--- src/main/kotlin/com/example/backend/controller/UserController.kt
+++ src/main/kotlin/com/example/backend/controller/UserController.kt
@@ -5,12 +5,9 @@ import com.example.backend.domain.service.impl.UserServiceImpl
 import com.example.backend.dto.response.*
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
-import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
-import org.springframework.web.bind.annotation.ExceptionHandler
 import java.security.MessageDigest
 import java.util.Date
-import javax.servlet.http.HttpServletRequest
 import kotlin.random.Random
 
 @RestController
@@ -22,21 +19,27 @@ class UserController {
     private val charPool : List<Char> = ('a'..'z') + ('A'..'Z') + ('0'..'9')
 
     fun createToken(): String{
-        var randomString = (1..TOKEN_LENGTH)
+        return (1..TOKEN_LENGTH)
                 .map { _ -> Random.nextInt(0, charPool.size) }
                 .map(charPool::get)
                 .joinToString("")
+    }
 
-        return randomString
+    fun passwordToHash(password: String?): String{
+        return MessageDigest.getInstance("SHA-256")
+                .digest(password?.toByteArray())
+                .joinToString(separator="") {
+                    "%02x".format(it)
+                }
     }
 
     @PostMapping("api/v1/user/register")
     fun registUser(@RequestBody user: User): User {
-        var findByNameUsers = userServiceImpl.findByName(user.name)
+        val findByNameUsers = userServiceImpl.findByName(user.name)
         if(findByNameUsers.isNotEmpty()){
             throw CommonException("already exist user", HttpStatus.BAD_REQUEST)
         }
-        var findByEmailUsers = userServiceImpl.findByEmail(user.email)
+        val findByEmailUsers = userServiceImpl.findByEmail(user.email)
         if(findByEmailUsers.isNotEmpty()){
             throw CommonException("used email", HttpStatus.BAD_REQUEST)
         }
@@ -44,77 +47,81 @@ class UserController {
         user.createdAt = Date()
         user.updatedAt = Date()
         // パスワードをハッシュ化
-        user.password = MessageDigest.getInstance("SHA-256")
-                .digest(user.password?.toByteArray())
-                .joinToString(separator="") {
-                    "%02x".format(it)
-                }
+        user.password = passwordToHash(user.password)
         // トークンを作成
         user.token = createToken()
-        var saveUser = userServiceImpl.save(user)
-//        saveUser.password = null
+        val saveUser = userServiceImpl.save(user)
+        saveUser.id = null
+        saveUser.password = null
+        saveUser.createdAt = null
+        saveUser.updatedAt = null
         return saveUser
     }
 
     @PostMapping("api/v1/user/login")
     fun loginUser(@RequestBody user: User): User {
-        user.password = MessageDigest.getInstance("SHA-256")
-                .digest(user.password?.toByteArray())
-                .joinToString(separator="") {
-                    "%02x".format(it)
-                }
-        var findUser = userServiceImpl.findByEmailAndPassword(user.email, user.password)
+        user.password = passwordToHash(user.password)
+        val findUser = userServiceImpl.findByEmailAndPassword(user.email, user.password)
         // 見つからない場合の処理
         if(findUser.isEmpty()) {
             throw CommonException("email or password is wrong", HttpStatus.BAD_REQUEST)
         }
-        return findUser[0]
+
+        val responseUser = findUser[0]
+        responseUser.id = null
+        responseUser.password = null
+        responseUser.createdAt = null
+        responseUser.updatedAt = null
+
+        return responseUser
     }
 
     @GetMapping("api/v1/user")
     fun getUser(@RequestParam token: String): User{
-        var findUser = userServiceImpl.findByToken(token)
+        val findUser = userServiceImpl.findByToken(token)
 
         if(findUser.isEmpty()){
             throw CommonException("Invalid Token", HttpStatus.BAD_REQUEST)
         }
 
-        return findUser[0]
+        val responseUser = findUser[0]
+        responseUser.id = null
+        responseUser.password = null
+        responseUser.createdAt = null
+        responseUser.updatedAt = null
+
+        return responseUser
     }
 
     @DeleteMapping("api/v1/user")
     fun deleteUser(@RequestParam token: String): Boolean{
-        var findUser = userServiceImpl.findByToken(token)
+        val findUser = userServiceImpl.findByToken(token)
         if(findUser.isEmpty()){
             throw CommonException("Invalid Token", HttpStatus.BAD_REQUEST)
         }
-        var intId = findUser[0].id
+        val intId = findUser[0].id
         if (intId != null) {
             userServiceImpl.deleteUser(intId)
         }
 
-        var deletedUser = userServiceImpl.findByToken(token)
+        val deletedUser = userServiceImpl.findByToken(token)
         return deletedUser.isEmpty()
     }
 
     @RequestMapping(path = ["api/v1/user"], method = [RequestMethod.PUT, RequestMethod.PATCH])
     fun updateUser(@RequestParam token: String, @RequestBody user: User): User {
-        var findUser = userServiceImpl.findByToken(token)
+        val findUser = userServiceImpl.findByToken(token)
         if(findUser.isEmpty()){
             throw CommonException("Invalid Token", HttpStatus.BAD_REQUEST)
         }
 
         if(!user.name.isNullOrBlank()) findUser[0].name = user.name
         if(!user.password.isNullOrBlank()){
-            user.password = MessageDigest.getInstance("SHA-256")
-                    .digest(user.password?.toByteArray())
-                    .joinToString(separator="") {
-                        "%02x".format(it)
-                    }
+            user.password = passwordToHash(user.password)
             findUser[0].password = user.password
         }
         if(!user.email.isNullOrBlank()) {
-            var emailFindUser = userServiceImpl.findByEmail(user.email)
+            val emailFindUser = userServiceImpl.findByEmail(user.email)
             if(emailFindUser.isNotEmpty()){
                 throw CommonException("Email Used", HttpStatus.BAD_REQUEST)
             }
@@ -126,6 +133,12 @@ class UserController {
 
         userServiceImpl.updateUser(findUser[0])
 
-        return findUser[0]
+        val responseUser = findUser[0]
+        responseUser.id = null
+        responseUser.password = null
+        responseUser.createdAt = null
+        responseUser.updatedAt = null
+
+        return responseUser
     }
 }
