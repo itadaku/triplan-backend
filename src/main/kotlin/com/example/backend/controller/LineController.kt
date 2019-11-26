1fa8d244588847cb30a6df3d5e5fa34782ace538
yasu89 <19990967+yasu89@users.noreply.github.com>
2019-11-26 15:00:13 +0900

    駅データのサンプルを返す

--- src/main/kotlin/com/example/backend/controller/LineController.kt
+++ src/main/kotlin/com/example/backend/controller/LineController.kt
@@ -1,8 +1,11 @@
 package com.example.backend.controller
 
 import com.example.backend.domain.models.response.GetLinesResponse
+import com.example.backend.domain.models.response.GetStationsResponse
 import com.example.backend.domain.models.response.LineResponse
+import com.example.backend.domain.models.response.StationItem
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -28,6 +31,41 @@ class LineController {
         item3.id = 3
         item3.name = "中央線"
         res.lines += item3
+
+        return res
+    }
+
+    @GetMapping("api/v1/line/{line_id}/stations")
+    fun getStations(@PathVariable("line_id") line_id: Int): GetStationsResponse {
+        var res = GetStationsResponse()
+
+        if(line_id == 1){
+            var item1 = StationItem()
+            item1.id = 1
+            item1.name = "新宿"
+            res.stations += item1
+            var item2 = StationItem()
+            item2.id = 2
+            item2.name = "代々木"
+            res.stations += item2
+            var item3 = StationItem()
+            item3.id = 3
+            item3.name = "原宿"
+            res.stations += item3
+        }else if(line_id == 2){
+            var item4 = StationItem()
+            item4.id = 4
+            item4.name = "新宿"
+            res.stations += item4
+            var item5 = StationItem()
+            item5.id = 5
+            item5.name = "新小岩"
+            res.stations += item5
+            var item6 = StationItem()
+            item6.id = 6
+            item6.name = "平井"
+            res.stations += item6
+        }
 
         return res
     }
