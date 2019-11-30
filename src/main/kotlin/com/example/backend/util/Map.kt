fcf58ff056b75050732c0bb5f04ebc7a2a039725
HIraWT <m15123@g.metro-cit.ac.jp>
2019-11-30 22:43:42 +0900

    所要時間取得関数追加

--- src/main/kotlin/com/example/backend/util/Map.kt
+++ src/main/kotlin/com/example/backend/util/Map.kt
@@ -11,9 +11,7 @@ class Map {
     fun getRequiredTime(origin :String?, destination :String?): String {
         var reqUrl = "https://maps.googleapis.com/maps/api/directions/json"
         var apiKey = "AIzaSyDaWBYhWzeIYQyoi7qQqOUNPSsu85EyVmw"
-        println("$reqUrl?origin=$origin&destination=$destination&key=$apiKey")
-
-        ("$reqUrl?origin=$origin&destination=$destination&key=$apiKey").httpGet().response { _, response, result ->
+       ("$reqUrl?origin=$origin&destination=$destination&key=$apiKey").httpGet().response { _, response, result ->
             when (result) {
                 is Result.Success -> {
                     var resJSON = String(response.data)
