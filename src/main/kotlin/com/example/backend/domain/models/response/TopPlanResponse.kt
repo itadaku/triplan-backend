a402ab331d05f4f7519bbcc489d105147e5e2045
yasu89 <19990967+yasu89@users.noreply.github.com>
2019-12-01 23:59:12 +0900

    TopPlanのレスポンスを変更

--- src/main/kotlin/com/example/backend/domain/models/response/TopPlanResponse.kt
+++ src/main/kotlin/com/example/backend/domain/models/response/TopPlanResponse.kt
@@ -4,13 +4,6 @@ import javax.persistence.Entity
 import javax.persistence.Id
 
 data class TopPlanResponse (
-    var id: Int? = null,
-    var title: String? = null,
-    var image: String? = null,
-    var review: Double? = null,
-    var days_nights: Int? = null,
-    var min_budget: Int? = null,
-    var max_budget: Int? = null,
-    var number_of_people: Int? = null,
-    var purpose: List<String> = listOf()
+    var top_title: String? = null,
+    var plans: List<TopPlanItem> = mutableListOf()
 )
