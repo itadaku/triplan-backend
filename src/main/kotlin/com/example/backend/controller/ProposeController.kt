b0226c95a341fbc94fb9313a7bd5e864dd5b7fb4
yasu89 <19990967+yasu89@users.noreply.github.com>
2019-12-03 01:34:27 +0900

    対応するじゃらんAPIの都道府県コードを取得

--- src/main/kotlin/com/example/backend/controller/ProposeController.kt
+++ src/main/kotlin/com/example/backend/controller/ProposeController.kt
@@ -10,6 +10,11 @@ import com.example.backend.domain.models.util.PrefectureForSort
 import com.example.backend.domain.service.impl.PrefectureServiceImpl
 import com.example.backend.domain.service.impl.UserServiceImpl
 import com.example.backend.dto.response.CommonException
+import com.github.kittinunf.fuel.httpGet
+import com.github.kittinunf.result.Result
+import org.json.JSONArray
+import org.json.JSONObject
+import org.json.XML
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
@@ -78,15 +83,61 @@ class ProposeController {
         return res
     }
 
+    fun searchPrefectureCode(prefName: String): String {
+        // 都道府県リストを取得してpref_codeを取得
+        var pref_code = ""
+        val areaSearchUrl = "http://jws.jalan.net/APICommon/AreaSearch/V1/"
+        val areaSearchParamList: HashMap<String, String> = hashMapOf("key" to "vir16ec73edea4")
+        val (_,response,resultAreaSearch) = areaSearchUrl.httpGet(areaSearchParamList.toList()).response()
+        when(resultAreaSearch) {
+            is Result.Success -> {
+                val json = XML.toJSONObject(String(response.data))
+                val jsonResults = json.get("Results") as JSONObject
+                val jsonArea = jsonResults.get("Area") as JSONObject
+                val jsonRegion = jsonArea.get("Region") as JSONArray
+                for(nowRegion in jsonRegion) {
+                    nowRegion as JSONObject
+
+                    if(nowRegion.get("name").toString() == "北海道" || nowRegion.get("name").toString() == "沖縄") {
+                        // 北海道と沖縄だけ例外処理
+                        val jsonPrefecture = nowRegion.get("Prefecture") as JSONObject
+                        if(jsonPrefecture.get("name").toString() == prefName){
+                            pref_code = jsonPrefecture.get("cd").toString()
+                        }
+                    }else{
+                        // その他都道府県
+                        val jsonPrefecture = nowRegion.get("Prefecture") as JSONArray
+                        for(nowPrefecture in jsonPrefecture) {
+                            nowPrefecture as JSONObject
+                            if(nowPrefecture.get("name").toString() == prefName){
+                                pref_code = nowPrefecture.get("cd").toString()
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return pref_code
+    }
+
     @GetMapping("api/v1/propose/2")
-    fun proposePlan(@RequestParam token: String, @RequestBody body: ProposePlanBody) {
+    fun proposePlan(@RequestParam token: String, @RequestBody body: ProposePlanBody) : String{
         // TOKENの確認
         val findUser = userServiceImpl.findByToken(token)
         if(findUser.isEmpty()){
             throw CommonException("Invalid Token", HttpStatus.BAD_REQUEST)
         }
 
+        // 都道府県の名前を取得
+        val searchPref = prefectureServiceImpl.findById(body.prefecture_id!!)
 
+        // pref_codeを利用して温泉を検索
+        val prefCode = searchPrefectureCode(searchPref.name!!)
+
+        println(prefCode)
+
+        return "Success"
     }
 
     @GetMapping("api/v1/propose/tags")
